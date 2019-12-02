1a5548f1734fb214c7e0cc126be7fe887d174ad6
Minliao Li <minliao@umail.ucsb.edu>
2019-12-02 08:49:14 -0800

    Add bar chart ML

--- client_src/src/components/GetOffer.js
+++ client_src/src/components/GetOffer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import{Chart} from 'chart.js';
 
 class GetOffer extends Component {
     constructor(props){
@@ -10,9 +11,62 @@ class GetOffer extends Component {
         }
     }
     componentDidMount(){
-        this.getMeetup();
+        this.getOffers();
     }
-    getMeetup(){
+    getOffers(){
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: ['Sports','Technology','Politics','Cooking','Pets'],
+            datasets: [{
+                label: 'trending rate',
+                data: [12, 19, 3, 5, 3, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
         let theTypes=this.props.match.params.class.split("+");
         //let className=this.props.match.params.class;
         var link=`http://localhost:3000/api/OneOffers?filter={"where":{"or":[`;
@@ -46,14 +100,27 @@ class GetOffer extends Component {
                 </div>
             )
         })
+
+        var divStyle = {
+            textalign:"center",
+            width:"1000px" ,
+            height:"400px"
+          };
         return (
             <div>
-                <br/>
-                <Link className="btn btn-success" to="/"> Back</Link>
-                <h2>{this.props.match.params.class} Offers:</h2>
+                <br></br>
+                <Link className="btn-lg btn-success" to="/"> Back</Link>
+                <div className="text-center">
+                    {/* <Link className="btn-lg btn-success" to="/"> Back</Link> */}
+                    <h2>{this.props.match.params.class} Offers:</h2>
+                </div>
+                <br></br>
                 <ul className="collection">
                     {MyOffers}
                 </ul>
+                <div className="myChartContainer" style={divStyle}>
+                <canvas id="myChart" width="1000" height="400"></canvas>
+                </div>
             </div>
         )
     }
