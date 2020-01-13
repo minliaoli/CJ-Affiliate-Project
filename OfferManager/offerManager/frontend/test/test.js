60a6eb048f55ded64bca5dea52d98fcb0c2ca80c
Kyle Ng <KyleNg@Kyles-MacBook-Pro.local>
2020-01-12 21:27:30 -0800

    Kyle and Nathan working on enzyme testing

--- OfferManager/offerManager/frontend/test/test.js
+++ OfferManager/offerManager/frontend/test/test.js
@@ -6,6 +6,7 @@ import sinon from 'sinon';
 import MyComponent from './MyComponent';
 import Foo from './Foo';
 
+/*
 describe('<MyComponent />', () => {
   it('renders three <Foo /> components', () => {
     const wrapper = shallow(<MyComponent />);
@@ -31,5 +32,55 @@ describe('<MyComponent />', () => {
     const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
     wrapper.find('button').simulate('click');
     expect(onButtonClick).to.have.property('callCount', 1);
+  });
+});
+*/
+
+//Template code for enzyme testing
+
+describe('<Foo />', () => {
+  it('allows us to set props', () => {
+    const wrapper = mount(<Foo bar="baz" />);
+    expect(wrapper.props().bar).to.equal('baz');
+    wrapper.setProps({ bar: 'foo' });
+    expect(wrapper.props().bar).to.equal('foo');
+  });
+
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount((
+      <Foo onButtonClick={onButtonClick} />
+    ));
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
+
+  it('calls componentDidMount', () => {
+    sinon.spy(Foo.prototype, 'componentDidMount');
+    const wrapper = mount(<Foo />);
+    expect(Foo.prototype.componentDidMount).to.have.property('callCount', 1);
+    Foo.prototype.componentDidMount.restore();
+  });
+});
+
+describe('<Foo />', () => {
+  it('renders three `.foo-bar`s', () => {
+    const wrapper = render(<Foo />);
+    expect(wrapper.find('.foo-bar')).to.have.lengthOf(3);
+  });
+
+  it('renders the title', () => {
+    const wrapper = render(<Foo title="unique" />);
+    expect(wrapper.text()).to.contain('unique');
+  });
+
+  it('renders the first tab', () => {
+    const wrapper = render(<Foo tab="unique" />);
+    expect(wrapper.text()).to.contain('unique');
+  });
+
+  it('renders the second tab', () => {
+    const wrapper = render(<Foo tab="unique" />);
+    expect(wrapper.text()).to.contain('unique');
   });
 });
