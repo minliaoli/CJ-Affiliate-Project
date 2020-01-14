9ed5d6a0af5affb783bead81a1b8f0d2b1cf9150
minliaoli <minliao@umail.ucsb.edu>
2020-01-14 15:10:31 -0800

    Open/close tabs completeted

--- client_src/src/pages/About.js
+++ client_src/src/pages/About.js
@@ -13,6 +13,28 @@ class About extends React.Component {
           <div className="App-container">
             <h2>This is About!</h2>
           </div>
+
+
+          <p>
+            <a class="btn btn-success btn-block" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Link with href
+            </a>
+          </p>
+
+          <div class="collapse" id="collapseExample">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+
+          <div className="tab">
+            <p>
+              <button class="btn btn-success btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                  Button with data-target
+              </button>
+            </p>
+          </div>
+
+
+
         </section>
 
 
--- client_src/src/pages/Home.js
+++ client_src/src/pages/Home.js
@@ -16,7 +16,39 @@ class Home extends React.Component {
             <div className="App-container">
               <h2 style={{textAlign: 'center'}}><strong>Select Your Blog Types:</strong></h2>
 
-              <BlogTypes />
+              <p>
+                <button class="btn btn-success btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+                  Enter by selecting from keywords
+                </button>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <BlogTypes />
+              </div>
+
+
+              <p>
+                <button class="btn btn-success btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                  Enter by entering blog text
+                </button>
+              </p>
+              <div class="collapse" id="collapseExample2">
+                <p>Text Area</p>
+              </div>
+
+
+
+              <p>
+                <button class="btn btn-success btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
+                  Enter by entering URL
+                </button>
+              </p>
+              <div class="collapse" id="collapseExample3">
+                <p>URL Area</p>
+              </div>
+
+
+
+
   
             </div>
           </section>
