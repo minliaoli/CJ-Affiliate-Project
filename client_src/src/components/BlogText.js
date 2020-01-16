7b577c881532644f3a94c23ea1828d6355a2274b
minliaoli <minliao@umail.ucsb.edu>
2020-01-15 20:09:54 -0800

    event handler for the two parts

--- client_src/src/components/BlogText.js
+++ client_src/src/components/BlogText.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 
 export class BlogText extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: ''
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Blog Text: ' + this.state.value);
+        event.preventDefault();
+    }
     render() {
         return (
-            <div class="form-group">
-                <label for="exampleFormControlTextarea4">Enter blog text:</label>
-                <textarea class="form-control" id="exampleFormControlTextarea4" rows="5"></textarea>
-                <br></br>
-                <button type="submit" class="btn btn-success mb-2">submit</button>
-            </div>                   
+            <form onSubmit={this.handleSubmit}>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea4">Enter blog text:</label>
+                    <textarea value={this.state.value} onChange={this.handleChange} 
+                    class="form-control" id="exampleFormControlTextarea4" rows="5" placeholder="Enter blog here"></textarea>
+                    <br></br>
+                    <button type="submit" class="btn btn-success mb-2">submit</button>
+                </div>    
+            </form>
+               
         )
     }
 }
--- client_src/src/components/BlogUrl.js
+++ client_src/src/components/BlogUrl.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 export class BlogUrl extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleSubmit(event) {
+        alert('URL: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                 <label htmlFor="urlInput">Enter URL</label>
-                <input type="email" className="form-control" id="urlInput" aria-describedby="urlHelp" placeholder="Enter URL" />
+                <input type="url" className="form-control" id="urlInput" aria-describedby="urlHelp" 
+                value={this.state.value} onChange={this.handleChange} placeholder="Enter URL here"/>
                 <small id="urlHelp" className="form-text text-muted">The blog might have access protection, if so please use text</small>
                 </div>
                 
