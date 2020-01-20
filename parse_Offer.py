6fbdd34650ddf5b61f954c73453a35695143d9bf
phosphorylation <38056156+phosphorylation@users.noreply.github.com>
2020-01-20 15:16:23 -0800

    Add files via upload

--- parse_Offer.py
+++ parse_Offer.py
@@ -14,94 +14,79 @@ class key_score(object):
 class offer(object):
     def __init__(self, text):
         self.text = text
-        self.key_scores = []
+        self.description = []
+        self.title = []
 
-    def addPair(self, keysc):
-        self.key_scores.append(keysc)
+    def addPairD(self, keysc):
+        self.description.append(keysc)
 
-    def getPairs(self):
-        return self.key_scores
+    def addPairT(self, keysc):
+        self.title.append(keysc)
+
+    def getPairsD(self):
+        return self.description
+
+    def getPairsT(self):
+        return self.title
 
     def getText(self):
         return self.text
 
 
-def Initialize_AWS():
+def Parse_Offer(filename):
+    Offerlist = []
     comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-
-def Parse_Offer(text):
-    keywordlist = []
-    comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-    #with open("capstone_shopping.json", 'r') as load_f:
-    #OfferData = json.load(load_f)
-    #for x in OfferData['products']:
-    text2 = text['title'] #+ text['description']
-    print('Calling detectingkeywords')
-    OutPutFAWS = comprehend.detect_key_phrases(Text=text2, LanguageCode='en')
-    print('End of detectingkeywords')
-    keyPhrases = OutPutFAWS['KeyPhrases']
-    for segment in keyPhrases:
-        keyword = segment['Text']
-        Score = segment['Score']
-        if(Score < 60):
-            continue;
-        keywordlist.append(keyword)
-    return keywordlist
-        
-                #pair = key_score(Score, keyword)
-                #newoffer.addPair(pair)
+    with open(filename, 'r') as load_f:
+        OfferData = json.load(load_f)
+        for x in OfferData['products']:
+            textTitle = x['title']
+            textDescription = x['description']
+            print('Calling detectingkeywords')
+            OutPutFAWS = comprehend.detect_key_phrases(Text=textTitle, LanguageCode='en')
+            OutPutFAWS2 = comprehend.detect_key_phrases(Text=textDescription, LanguageCode='en')
+            print('End of detectingkeywords')
+            newoffer = offer(x)
+            keyPhrases = OutPutFAWS['KeyPhrases']
+            #print keyPhrases
+            for segment in keyPhrases:
+                keyword = segment['Text']
+                Score = segment['Score']
+                keyword.encode("ascii")
+                pair = key_score(Score, keyword)
+                newoffer.addPairT(pair)
                 #print  newoffer.getPairs()[0]
-            #Offerlist.append(newoffer)
-            #break
+            keyPhrases = OutPutFAWS2['KeyPhrases']
+            for segment in keyPhrases:
+                keyword = segment['Text']
+                Score = segment['Score']
+                pair = key_score(Score, keyword)
+                newoffer.addPairD(pair)
+            Offerlist.append(newoffer)
+            break
 
-    
     ## this segment of loop test the functionality of printing the organized
     # keywords in an offer
-    #print Offerlist[0].getText()
-    #for x in Offerlist[0].getPairs():
-    #   print x
-
-
-def Parse_Blog(text):
-    keywordlist = []
-    comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-    #with open("capstone_shopping.json", 'r') as load_f:
-    #OfferData = json.load(load_f)
-    #for x in OfferData['products']:
-    print('Calling detectingkeywords')
-    OutPutFAWS = comprehend.detect_key_phrases(Text=text, LanguageCode='en')
-    print('End of detectingkeywords')
-    keyPhrases = OutPutFAWS['KeyPhrases']
-    for segment in keyPhrases:
-        keyword = segment['Text']
-        Score = segment['Score']
-        if(Score < 60):
-            continue;
-        keywordlist.append(keyword)
-    return keywordlist
-
-def analyze_all_Offers():
-    #someway to access all the offers
-    for x in OfferData['products']:
-        keywords = Parse_Offer(x)
-        #someway to create a new entry in mongoDB
-
-
-def get_tag(text):
-    
-        
-
-
-
-##########detecting entities
+    print Offerlist[0].getText()
+    print 'TITLE'
+    for x in Offerlist[0].getPairsT():
+        print x
+    print 'Description'
+    for x in Offerlist[0].getPairsD():
+        print x
+    ##########detecting entities
     print('Calling DetectEntities')
-    print(json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+    print(json.dumps(comprehend.detect_entities(Text=textDescription, LanguageCode='en'), sort_keys=True, indent=4))
     print('End of DetectEntities\n')
 
+####there are lots of redundencies in offers, maybe we can clean them
+def Clean_Offers():
+    with open("capstone_all.json", 'r') as load_f:
+        OfferData = json.load(load_f)
+
+
+
+
 if __name__ == "__main__":
-    Parse_Offer(text)
-    """
-    You can use this to test your code.
-    python hw2.py [training file path] [testing file path]
-    """
+    Parse_Offer("capstone_shopping.json")
+
 
---
+++ synonym.py
@@ -0,0 +1,21 @@
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+def check(keywordO,keywordB):
+    lemmatizer = WordNetLemmatizer()
+    g = lemmatizer.lemmatize('Cardigan',pos='v')
+    print  g
+    w1 = wordnet.synset('Cardigan.n.01')
+    w2 = wordnet.synset('car.n.01')
+    print(w1.wup_similarity(w2))
+    # for O in keywordO:
+    #     for B in keywordB:
+    #         word=O+'.n.01'
+    #         word2=B+'.n.01'
+    #         prep = wordnet.synset(word2)
+    #         prep2 = wordnet.synset(word)
+    #         print(prep.wup_similarity(prep2))
+
+
+if __name__ == "__main__":
+    check(1,2)
