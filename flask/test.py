fbdebbc55625d350e3f88d2ca59bfed175477aa8
Nathan Guan <nathanguan@169-231-115-192.wireless.ucsb.edu>
2020-01-20 17:09:45 -0800

    fixed test file and added to alg skeleton code

--- flask/test.py
+++ flask/test.py
@@ -16,27 +16,31 @@ class MyTest(LiveServerTestCase):
 
     def test_home_page_status_code(self):
         response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.code, 200)
 
     def test_first_test_page_status_code(self):
         response = self.client.get('/testAlg')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.code, 200)
+        
 
     def test_extended_input_page_status_code(self):
         response = self.client.get('/test/data%20is%20fun')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.code, 200)
+        self.assertEquals(response.data, 'true')
 
     def test_extended_input_page_status_code_2(self):
         response = self.client.get('/test/data is fun')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.code, 200)
+        self.assertEquals(response.data, 'true')
 
     def test_input_page_status_code(self):
         response = self.client.get('/test/data')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.code, 200)
+        self.assertEquals(response.data, 'yes but')
 
     def test_alg_page_status_code(self):
         response = self.client.get('/alg/testInput')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.code, 200)
 
 
         
