bf9f2556a73fbc39519025f2ef2807a659ba9d96
Joey Zhang <35642308+ZCY4014@users.noreply.github.com>
2020-02-07 15:40:07 -0800

    Add files via upload

--- categorize.py
+++ categorize.py
@@ -5,7 +5,18 @@ from pymongo import MongoClient
 import datetime
 import pprint
 import json
-import re
+
+trendweight = {}
+trendweight["Fashion"]= 1
+trendweight["Recreation"] = 1
+trendweight["Sports"] = 1
+trendweight["Games"] = 1
+trendweight["Health"] = 1
+trendweight["Home"] = 1
+trendweight["Business"] = 1
+trendweight["Technology"] = 1
+trendweight["Society"] = 1
+trendweight["Computers"] = 1
 
 def initializeDB():
     client = MongoClient()
@@ -17,61 +28,74 @@ def sortSecond(val):
     a = list(val)
     return a[1]
 
+def trend_weight(list):
+    i = 0
+    for each in list:
+        if i ==3:
+            break
+        trendweight[each[0]] += each[1]
+        i += 1
+
+def parse_trend(trends):
+    return None
+
 def function_a(text):
+    # this is the real deal
     scores = categorize(text)
+    f = open("capstone_shopping_clean.json.json", "r")
+    OfferData = json.load(f)
+    similarity = []
+    ##############Get Trend
+    ##############Parse Trend
+    ############## Trend weight
+
+    for each in OfferData:
+        score = 0
+        if list(scores[0])[0] == each['categoryname1']:
+            weight = trendweight[list(scores[0])[0]]
+            score += list(scores[0])[1] * 1 * weight
+        if list(scores[1])[0] == each['categoryname1']:
+            weight = trendweight[list(scores[1])[0]]
+            score += list(scores[1])[1] * 1* weight
+        if list(scores[2])[0] == each['categoryname1']:
+            weight = trendweight[list(scores[2])[0]]
+            score += list(scores[2])[1] * 1* weight
+        if list(scores[0])[0] == each['categoryname2']:
+            weight = trendweight[list(scores[0])[0]]
+            score += list(scores[0])[1] * 0.8* weight
+        if list(scores[1])[0] == each['categoryname2']:
+            weight = trendweight[list(scores[1])[0]]
+            score += list(scores[1])[1] * 0.8* weight
+        if list(scores[2])[0] == each['categoryname2']:
+            weight = trendweight[list(scores[2])[0]]
+            score += list(scores[2])[1] * 0.8* weight
+        if list(scores[0])[0] == each['categoryname3']:
+            weight = trendweight[list(scores[0])[0]]
+            score += list(scores[0])[1] * 0.6* weight
+        if list(scores[1])[0] == each['categoryname3']:
+            weight = trendweight[list(scores[1])[0]]
+            score += list(scores[1])[1] * 0.6* weight
+        if list(scores[2])[0] == each['categoryname3']:
+            weight = trendweight[list(scores[2])[0]]
+            score += list(scores[2])[1] * 0.6* weight
+        similarity.append([each, score])
+    similarity.sort(key=sortSecond, reverse=True)
+    return similarity
 
 
 
 def funuction_b():
     #this function categorizes every offers on the database
-    new_dict ={}
-    new_dict["products"] = []
-    f =open("capstone_all_with_pics_clean.json","r")
-    OfferData = json.load(f)
-    i =0
-    for OneLine in OfferData["products"]:
-        if i < 4000:
-            i += 1
-            continue
-        if i >= 6000:
-            break
-        text =OneLine['title']+" "+OneLine['title']+" "+OneLine['title']+" "+OneLine['description']
-        new = text.encode('utf-8')
-        cleanString = re.sub('\W+', ' ', new)
-        response = requests.post('https://api.uclassify.com/v1/uclassify/Topics/classify', \
-                                 data="{\"texts\": [\"%s\"]}" % (cleanString), \
-                                 headers={'Authorization': 'Token ' + "QFlcgwbjS2P9"})
-        response = response.json()
-        responseCat = response[0]['classification']
-        scores = []
-        for each in responseCat:
-            if each['className'] == 'Science':
-                each['className'] = 'Technology'
-            if each['className'] == 'Arts':
-                each['className'] = 'Fashion'
-
-            scores.append(each.values())
-        scores.sort(key=sortSecond, reverse=True)
-        #del OneLine["categoryName"]
-        OneLine["categoryName1"] = str(scores[0][0])
-        OneLine["categoryName2"] = str(scores[1][0])
-        OneLine["categoryName3"] = str(scores[2][0])
-        i +=1
-        new_dict["products"].append(OneLine)
-    new_dict2= json.dumps(new_dict)
-    with open("capstone_all_with_pics_clean4000-5100.json", "a+") as f:
-        f.write(str(new_dict2))
-        f.close()
     return None
 
 
 def function_c(text):
     #this function is a local demonstration
     scores = categorize(text)
-    f =open("capstone_all_with_pics_clean3.json","r")
+    f =open("local.json","r")
     OfferData = json.load(f)
     similarity=[]
-    for each in OfferData['products']:
+    for each in OfferData:
         score = 0
         if list(scores[0])[0] == each['categoryname']:
             score += list(scores[0])[1]*1
@@ -81,7 +105,6 @@ def function_c(text):
             score += list(scores[2])[1] * 0.6
         similarity.append([each,score])
     similarity.sort(key=sortSecond,reverse=True)
-    similarity= json.dumps(similarity)
     return similarity
 
 
@@ -110,5 +133,4 @@ def categorize(text):
 if __name__ == "__main__":
     string ="sdf {} sdd".format("\"qee\"")
     print  (string)
-    funuction_b()
-    #print (function_c("The car/van picks you up from Noi Bai airport (in Ha Noi) to take you to the heart of the city. After arriving at the hotel after a 40-minute drive, check in and have a free time to walk around Old Quarter or explore the night market in Ha Noi downtown.Until 21:00 pm: Our guide will pick up you at your hotel for boarding the train to travel to Lao Cai."))
+    print (function_c("The car/van picks you up from Noi Bai airport (in Ha Noi) to take you to the heart of the city. After arriving at the hotel after a 40-minute drive, check in and have a free time to walk around Old Quarter or explore the night market in Ha Noi downtown.Until 21:00 pm: Our guide will pick up you at your hotel for boarding the train to travel to Lao Cai."))
