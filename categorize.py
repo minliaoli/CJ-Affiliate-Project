555c71b5394566c05e483bde42dcc4b939c19323
phosphorylation <38056156+phosphorylation@users.noreply.github.com>
2020-02-10 20:08:54 -0800

    Add files via upload

--- categorize.py
+++ categorize.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 import datetime
 import pprint
 import json
-
+import random
 trendweight = {}
 trendweight["Fashion"]= 1
 trendweight["Recreation"] = 1
@@ -36,46 +36,61 @@ def trend_weight(list):
         trendweight[each[0]] += each[1]
         i += 1
 
+def trend_scrambller():
+    for key in trendweight.keys():
+        scrambler = random.random()*0.2-0.1
+        trendweight[key] = trendweight[key]*(1+scrambler)
+
 def parse_trend(trends):
     return None
+
+def hard_work():
+    f = open("categorized_clean.json", "r")
+    Offerdata = f.readlines()
+    f.close()
+    f2 = open("categorized_clean2.json", "w")
+    for each in Offerdata:
+        f2.write(str(each)+","+"\n")
+    f2.close()
+
 
 def function_a(text):
     # this is the real deal
     scores = categorize(text)
-    f = open("capstone_shopping_clean.json.json", "r")
+    f = open("categorized_clean2.json", "r")
     OfferData = json.load(f)
     similarity = []
     ##############Get Trend
     ##############Parse Trend
     ############## Trend weight
 
-    for each in OfferData:
+    for each in OfferData["products"]:
         score = 0
-        if list(scores[0])[0] == each['categoryname1']:
+        if list(scores[0])[0] == each['categoryName1']:
             weight = trendweight[list(scores[0])[0]]
             score += list(scores[0])[1] * 1 * weight
-        if list(scores[1])[0] == each['categoryname1']:
+        if list(scores[1])[0] == each['categoryName1']:
             weight = trendweight[list(scores[1])[0]]
             score += list(scores[1])[1] * 1* weight
-        if list(scores[2])[0] == each['categoryname1']:
+        if list(scores[2])[0] == each['categoryName1']:
             weight = trendweight[list(scores[2])[0]]
             score += list(scores[2])[1] * 1* weight
-        if list(scores[0])[0] == each['categoryname2']:
+        if list(scores[0])[0] == each['categoryName2']:
             weight = trendweight[list(scores[0])[0]]
             score += list(scores[0])[1] * 0.8* weight
-        if list(scores[1])[0] == each['categoryname2']:
+        if list(scores[1])[0] == each['categoryName2']:
             weight = trendweight[list(scores[1])[0]]
             score += list(scores[1])[1] * 0.8* weight
-        if list(scores[2])[0] == each['categoryname2']:
+        if list(scores[2])[0] == each['categoryName2']:
             weight = trendweight[list(scores[2])[0]]
             score += list(scores[2])[1] * 0.8* weight
-        if list(scores[0])[0] == each['categoryname3']:
+        if list(scores[0])[0] == each['categoryName3']:
             weight = trendweight[list(scores[0])[0]]
             score += list(scores[0])[1] * 0.6* weight
-        if list(scores[1])[0] == each['categoryname3']:
+        if list(scores[1])[0] == each['categoryName3']:
             weight = trendweight[list(scores[1])[0]]
             score += list(scores[1])[1] * 0.6* weight
-        if list(scores[2])[0] == each['categoryname3']:
+        if list(scores[2])[0] == each['categoryName3']:
             weight = trendweight[list(scores[2])[0]]
             score += list(scores[2])[1] * 0.6* weight
         similarity.append([each, score])
@@ -85,8 +100,44 @@ def function_a(text):
 
 
 def funuction_b():
-    #this function categorizes every offers on the database
-    return None
+    new_dict = {}
+    new_dict["products"] = []
+    f = open("capstone_all_with_pics_clean.json", "r")
+    OfferData = json.load(f)
+    i = 0
+    for OneLine in OfferData["products"]:
+        if i < 4000:
+            i += 1
+            continue
+        if i >= 6000:
+            break
+        text = OneLine['title'] + " " + OneLine['title'] + " " + OneLine['title'] + " " + OneLine['description']
+        new = text.encode('utf-8')
+        cleanString = re.sub('\W+', ' ', new)
+        response = requests.post('https://api.uclassify.com/v1/uclassify/Topics/classify', \
+                                 data="{\"texts\": [\"%s\"]}" % (cleanString), \
+                                 headers={'Authorization': 'Token ' + "QFlcgwbjS2P9"})
+        response = response.json()
+        responseCat = response[0]['classification']
+        scores = []
+        for each in responseCat:
+            if each['className'] == 'Science':
+                each['className'] = 'Technology'
+            if each['className'] == 'Arts':
+                each['className'] = 'Fashion'
+
+            scores.append(each.values())
+        scores.sort(key=sortSecond, reverse=True)
+        # del OneLine["categoryName"]
+        OneLine["categoryName1"] = str(scores[0][0])
+        OneLine["categoryName2"] = str(scores[1][0])
+        OneLine["categoryName3"] = str(scores[2][0])
+        i += 1
+        new_dict["products"].append(OneLine)
+    new_dict2 = json.dumps(new_dict)
+    with open("capstone_all_with_pics_clean4000-5100.json", "a+") as f:
+        f.write(str(new_dict2))
+    f.close()
 
 
 def function_c(text):
@@ -132,5 +183,9 @@ def categorize(text):
 
 if __name__ == "__main__":
     string ="sdf {} sdd".format("\"qee\"")
-    print  (string)
-    print (function_c("The car/van picks you up from Noi Bai airport (in Ha Noi) to take you to the heart of the city. After arriving at the hotel after a 40-minute drive, check in and have a free time to walk around Old Quarter or explore the night market in Ha Noi downtown.Until 21:00 pm: Our guide will pick up you at your hotel for boarding the train to travel to Lao Cai."))
+    #hard_work()
+    # print  (string)
+    trend_scrambller()
+    list = function_a(" ought to be talking about Verona. We ought to be talking about an astonishing week for the club with the lowest wage bill in Serie A, in which they drew with Milan and Lazio before beating the champions, and league leaders, Juventus. We ought to be talking about how a newly promoted side went from battling relegation to sixth place, via an eight-game unbeaten run, inspired by a manager with a passion for death metal.")
+    for i in range (0,20):
+        print (list[i])
